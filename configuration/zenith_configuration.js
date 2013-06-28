bbacb3c48fe604b33a68b3b383117febaead820a
chamilaadhi <cka.adhi@gmail.com>
2013-06-28 21:06:04 +0530

    configuration files

---
+++ configuration/zenith_configuration.js
@@ -0,0 +1,41 @@
+/**
+ * this is the configuration file. message object has the message body in
+ * message.body field. 
+ * callback function should be called with the result. this result is sent
+ * back to the request
+ */
+
+var logger = require('../lib/logger');
+function mediate(message, callback){	
+
+	
+	var serviceURL = 'http://localhost:9000/service/EchoService';
+	var pathName = message.transportHeaders.url.pathname;
+	
+	
+	//direct proxy
+	if(pathName === '/services/DirectProxy'){
+		
+		callWebService(message, serviceURL, callback)
+	} 
+	
+	//CBR proxy
+	
+	
+}
+exports.mediate = mediate;
+
+function callWebService(message, serviceURL, callback){
+	
+		var option = {
+			url : serviceURL//'http://localhost:9000/services/SimpleStockQuoteService'
+				//'http://localhost:9000/service/EchoService'
+		};
+			
+		var endpoint = require('../lib/support/ws_endpoint');
+		
+		endpoint.callService(message, option, function(err,message){		
+			callback(null, message.body);		
+		});	
+}
+
