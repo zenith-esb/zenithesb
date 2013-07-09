4ca65b40b782d4213829d1d169831e8242c3074a
Chamila Adhikarinayake <cka.adhi@gmail.com>
2013-07-09 13:56:26 +0530

    added clustering to start esb on all the cpu core

--- zenith.js
+++ zenith.js
@@ -1,6 +1,7 @@
 /**
  * Entry point for the Zenith ESB
  */
+var cluster = require('cluster')
 var logger = require('./lib/logger');
 var MessageFormatter = require('./lib/message_formatter');
 var messageFormatter = new MessageFormatter();
@@ -15,10 +16,47 @@ var TRANSPORT_PATH = './lib/transport/';
 /*
  * following part is hard coded to recieve http requests
  */
-logger.info('Zenith', 'Starting Zenith ESB .....');
-var HttpTransport = require( TRANSPORT_PATH + 'http_transport');
 
-var config = serverConfig.httpServerConfig;
 
-var httpTransport = new HttpTransport(config, messageFormatter);
-httpTransport.start(); //start the listner
+//Code to run if we're in the master process
+if (cluster.isMaster) {
+  // Count the machine's CPUs
+	logger.info('Zenith', 'Starting Zenith ESB .....');
+    var cpuCount = require('os').cpus().length;
+    logger.info('Zenith', 'Starting process(es) on ' + cpuCount +
+    		' core(s)');
+    // Create a worker for each CPU
+    for (var i = 0; i < cpuCount; i += 1) {
+        cluster.fork();
+    }
+
+    // Listen for dying workers
+    cluster.on('exit', function (worker) {
+
+        // Replace the dead worker,
+    	logger.info('Zenith', 'Worker ' + worker.id + ' died');
+    	cluster.fork();
+
+    });
+
+// Code to run if we're in a worker process
+} else {
+	logger.info('Zenith', 'Starting process on CPU ' + cluster.worker.id );
+	startZenithESB();
+}
+
+/**
+ * start an instance of the ESB
+ */
+function startZenithESB(){
+	//@TODO currently hardcoded to accept HTTP connections
+	//		change this to a generic form to add new transports
+	
+	var HttpTransport = require( TRANSPORT_PATH + 'http_transport');
+
+	var config = serverConfig.httpServerConfig;
+
+	var httpTransport = new HttpTransport(config, messageFormatter);
+	httpTransport.start(); //start the listner
+	
+}
