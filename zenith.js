9ef197dc6162bae4f99ba2013bf9ccfe24dcc995
Chamila Adhikarinayake <cka.adhi@gmail.com>
2013-09-07 13:04:02 +0530

    starting the amqp server

--- zenith.js
+++ zenith.js
@@ -5,19 +5,27 @@ var cluster = require('cluster');
 var logger = require('./lib/logger');
 var MessageFormatter = require('./lib/message_formatter');
 var transportFactory = require('./lib/transport/transport_factory');
+var messagingFactory = require('./lib/amqp/messaging_factory');
 var messageFormatter = new MessageFormatter();
 var serverConfig = require('./configuration/server_config');
 var TRANSPORT_PATH = './lib/transport/';
 var app = require('./lib/monitor/app.js')
-
+var msgServerStarted = false;
 
 
 //Code to run if we're in the master process
 if (cluster.isMaster) {
   // Count the machine's CPUs
-	logger.info('Zenith', 'Starting Zenith ESB .....');
+	logger.info('Zenith', ' Starting Zenith ESB .....');
     var cpuCount = require('os').cpus().length;
-    logger.info('Zenith', 'Starting process(es) on ' + cpuCount +
+
+	//starting messaging server
+	if(!msgServerStarted){
+		messagingFactory.initialize();
+		msgServerStarted = true;
+	}
+	
+    logger.info('Zenith', ' Starting process(es) on ' + cpuCount +
     		' core(s)');
     		
     // Create a worker for each CPU
@@ -29,14 +37,14 @@ if (cluster.isMaster) {
     cluster.on('exit', function (worker) {
 
         // Replace the dead worker,
-    	logger.info('Zenith', 'Worker ' + worker.id + ' died');
+    	logger.info('Zenith', ' Worker ' + worker.id + ' died');
     	cluster.fork();
 
     });
 
 // Code to run if we're in a worker process
 } else {
-	logger.info('Zenith', 'Starting process on CPU ' + cluster.worker.id );
+	logger.info('Zenith', ' Starting process on CPU ' + cluster.worker.id );
 	startZenithESB();
 }
 
@@ -51,6 +59,8 @@ function startZenithESB(){
 		var server = transports[x].start();
 		app.monitor.Monitor(server,{'collect_all': 'yes', 'top':{'view':3,'limit':100, 'timelimit':1, 'sortby': 'max_time'}});//add server to monitor
 	}
+	
+	
 	app.startDashBoard();
 	
 }
