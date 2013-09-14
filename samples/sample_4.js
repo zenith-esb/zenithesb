2150a49d8638cf3341225c91c356b6363f6b298f
chamilaadhi <cka.adhi@gmail.com>
2013-09-14 09:38:08 +0530

    add demo sample

--- samples/sample_4.js
+++ samples/sample_4.js
@@ -12,7 +12,47 @@ var logger = require('../lib/logger');
 var event = require(SUPPORT_LIBS + 'event');
 exports.executeSample = function(zenithMessage, callback){
 
-	var topic = '09.cse.uom';
-	event.publish(topic, zenithMessage, callback);
+	var topic; //= '09.cse.uom';
+	//event.publish(topic, zenithMessage, callback);
+	
+	var reqUrl = zenithMessage.transportHeaders.url; //returns url object
+
+	if(reqUrl.pathname === '/message/All'){
+		
+		topic = 'station.';
+		event.publish(topic, zenithMessage, callback);
+		
+	} else if(reqUrl.pathname === '/message/Fort'){
+		
+		topic = 'station.fort';
+		event.publish(topic, zenithMessage, callback);
+		
+	} else if(reqUrl.pathname === '/message/Gampaha'){
+		
+		topic = 'station.gampaha';
+		event.publish(topic, zenithMessage, callback);
+		
+	} else if(reqUrl.pathname === '/axis2/services/SeatService'){
+		
+		var serviceURL = 'http://192.168.105.1:9000/axis2/services/SeatService';
+		logger.debug('SampleConfig', 'EPR: ' + serviceURL);
+		
+		var option = {
+				url : serviceURL
+			};
+				
+		var endpoint = require(SUPPORT_LIBS + 'ws_endpoint');
+			
+		endpoint.callService(zenithMessage, option, function(err,message){		
+				callback(null, message);		
+			});	
+		
+	} else {
+		
+		var errMsg = 'Invalid EPR value.'; 
+		zenithMessage.body = soapErrorMsg.getSOAP11Fault(errMsg);
+		callback(null, zenithMessage);
+	}
+		
 
 }
