d11e7c64a95fe720771b918ef51e3d6154706635
Gimantha Bandara <gmnth.bndr@gmail.com>
2013-10-29 23:58:04 +0530

    sample file for testing xslt

---
+++ samples/sample_5.js
@@ -0,0 +1,47 @@
+/**
+ * XSLT transformation scenario
+ */
+
+var SUPPORT_LIBS = '../lib/support/';
+var XSLT_RES = '../resources/xslt/';
+var logger = require('../lib/logger');
+
+var soapErrorMsg = require('../lib/util/errormsg/soap_err_msg');
+
+exports.executeSample = function(zenithMessage, callback) {
+	var reqUrl = zenithMessage.transportHeaders.url; //returns url object
+
+	if (reqUrl.pathname === '/services/StockQuoteProxy') {
+
+		var serviceURL = 'http://localhost:9000/services/SimpleStockQuoteService';
+		logger.debug('SampleConfig', 'EPR: ' + serviceURL);
+
+		var option = {
+			url : serviceURL
+		};
+
+		var endpoint = require(SUPPORT_LIBS + 'ws_endpoint');
+		var xslt = require(SUPPORT_LIBS + 'xml/xslt_processor');
+
+		var xsltFile = XSLT_RES + 'transform.xslt';
+		var xsltFile_back = XSLT_RES + 'transform_back.xslt';
+
+		var transformedMsg = xslt
+				.transformMsg(zenithMessage.body, xsltFile, []);
+		zenithMessage.body = transformedMsg;
+
+		endpoint.callService(zenithMessage, option, function(err, message) {
+
+			var transformedBckMsg = xslt.transformMsg(message, xsltFile_back,
+					[]);
+			callback(null, transformedBckMsg);
+		});
+
+	} else {
+		var errMsg = 'Invalid EPR value.';
+		zenithMessage.body = soapErrorMsg.getSOAP11Fault(errMsg);
+		callback(null, zenithMessage);
+
+	}
+
+}
