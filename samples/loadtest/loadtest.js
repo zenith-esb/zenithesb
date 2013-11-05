937b3a8bd4e25412f1da305c58e110e4b1b09672
Chamila Adhikarinayake <cka.adhi@gmail.com>
2013-11-06 00:31:44 +0530

    fixed error condition

--- samples/loadtest/loadtest.js
+++ samples/loadtest/loadtest.js
@@ -10,7 +10,7 @@ var logger = require('../../lib/logger');
 
 exports.executeTest = function(zenithMessage, callback){
 	
-	var serviceURL = 'http://localhost:9000/service/EchoService';//real service url
+	var serviceURL = 'http://192.168.0.1:9000/service/EchoService';//real service url
 	var reqUrl = zenithMessage.transportHeaders.url; //returns url object
 
 	if(reqUrl.pathname === '/services/DirectProxy'){
@@ -35,7 +35,7 @@ exports.executeTest = function(zenithMessage, callback){
 		
 	} else {
 		//unknow request
-		var errMsg = oapErrorMsg.getSOAP11Fault('Invalid EPR value.'); 
+		var errMsg = soapErrorMsg.getSOAP11Fault('Invalid EPR value.'); 
 		var errZenithMessage = zenithErrorMsg.getZenithErrorMSG(errMsg, 'text/xml', '500');
 		callback(null, errZenithMessage);
 	}
@@ -105,7 +105,7 @@ function cbrSOAPHeaderProxy(zenithMessage, serviceURL, callback){
 				
 			} else {
 				//set the message body to the error message
-				var errMsg = oapErrorMsg.getSOAP11Fault('Message does not contain \'' + soapHeaderValue +'\'.'); 
+				var errMsg = soapErrorMsg.getSOAP11Fault('Message does not contain \'' + soapHeaderValue +'\'.'); 
 				var errZenithMessage = zenithErrorMsg.getZenithErrorMSG(errMsg, 'text/xml', '400');
 				callback(null, errZenithMessage);
 			}
@@ -142,12 +142,65 @@ function cbrTransportHeaderProxy(zenithMessage, serviceURL, callback){
 		
 	} else {
 		//set the message body to the error message
-		var errMsg = oapErrorMsg.getSOAP11Fault('Invalid routing header'); 
+		var errMsg = soapErrorMsg.getSOAP11Fault('Invalid routing header'); 
 		var errZenithMessage = zenithErrorMsg.getZenithErrorMSG(errMsg, 'text/xml', '400');
 		callback(null, errZenithMessage);
 	}
 }
 
+/**
+ * XSLT proxy
+ * @param zenithMessage
+ * @param serviceURL
+ * @param callback
+ */
 function xsltProxy(zenithMessage, serviceURL, callback){
+	var XSLT_RES = './resources/xslt/';
+	logger.debug('SampleConfig', 'EPR: ' + serviceURL);
+
+	var option = {
+		url : serviceURL
+	};
+
+	var endpoint = require(SUPPORT_LIBS + 'ws_endpoint');
+	var xslt = require(SUPPORT_LIBS + 'xml/xslt_processor');
+	var saxProcessor = require(SUPPORT_LIBS + 'xml/sax_processor');
 	
+	//files for transforming and transforming back the requests and responses respectively.
+	var xsltFile = XSLT_RES + 'transform_env_reverse.xslt';
+	var xsltFile_back = XSLT_RES + 'transform_env.xslt';
+	
+	//transform soap body to something that the real service can understand
+	var transformedMsg = xslt
+			.transformXML(zenithMessage.body, xsltFile, []);
+	
+	
+	// create the soap message using original soap headers and transformed soap body
+	saxProcessor.getTransformedSOAP(zenithMessage.body, transformedMsg, function(err, transformedSOAP){
+		
+		zenithMessage.body = transformedSOAP;
+		endpoint.callService(zenithMessage, option, function(err, message) { 
+			
+			if(!err){
+				// transformed the response so that the client can understand the format.
+				var transformedBckMsg = xslt.transformXML(message.body, xsltFile_back, []);
+				
+				saxProcessor.getTransformedSOAP(message.body, transformedBckMsg, function(err, transformedBckSOAP){
+					logger.debug('XSLTProxy','Response :'+transformedBckSOAP);
+					message.body = transformedBckSOAP;
+					callback(null, message);
+				});
+				
+			} else {
+				//send error message for external web service error
+				//can create more detailed errors by reading fields in 'err' object
+				var errMsg = soapErrorMsg.getSOAP11Fault('Connection Refused.'); 
+				var errZenithMessage = zenithErrorMsg.getZenithErrorMSG(errMsg, 'text/xml', '503');
+				callback(null, errZenithMessage);
+			}
+			
+		});
+	});
+	
+
 }
