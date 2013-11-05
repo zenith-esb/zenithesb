df8abe44b559dba64b0021f31063febad427d77c
Gimantha Bandara <gmnth.bndr@gmail.com>
2013-11-05 20:09:46 +0530

    Added XSLTProxy Loadtest sample

---
+++ resources/xslt/transform_env.xslt
@@ -0,0 +1,13 @@
+<?xml version="1.0" encoding="ISO-8859-1"?>
+<xsl:stylesheet version="1.0" xmlns:xsl="http://www.w3.org/1999/XSL/Transform" xmlns:m0="http://services.samples/xsd">
+<xsl:output method="xml" omit-xml-declaration="yes" indent="no" exclude-result-prefixes="m0"/>
+
+<xsl:template match="m0:skcotSyub">
+
+<m:buyStocks xmlns:m="http://services.samples/xsd">
+<xsl:for-each select="redro">
+<order><symbol><xsl:value-of select="lobmys"/></symbol><buyerID><xsl:value-of select="DIreyub"/></buyerID><price><xsl:value-of select="ecirp"/></price><volume><xsl:value-of select="emulov"/></volume></order>
+</xsl:for-each>
+</m:buyStocks>
+</xsl:template>        
+</xsl:stylesheet>
---
+++ samples/loadtest/xslt_proxy.js
@@ -0,0 +1,71 @@
+/**
+ * XSLTProxy - transform the soap body so that the service can understand 
+ * the request and client the response
+ */
+
+var SUPPORT_LIBS = '../../lib/support/';
+var XSLT_RES = './resources/xslt/';
+var logger = require('../../lib/logger');
+var soapErrorMsg = require('../../lib/util/errormsg/soap_err_msg');
+var zenithErrorMsg = require('../../lib/util/errormsg/zenith_err_msg');
+
+exports.executeTest = function(zenithMessage, callback) {
+	
+	var reqUrl = zenithMessage.transportHeaders.url; // returns url object
+	var serviceURL = 'http://localhost:9000/service/EchoService';
+	
+	if (reqUrl.pathname === '/services/XSLTProxy') {
+		
+		logger.debug('SampleConfig', 'EPR: ' + serviceURL);
+
+		var option = {
+			url : serviceURL
+		};
+
+		var endpoint = require(SUPPORT_LIBS + 'ws_endpoint');
+		var xslt = require(SUPPORT_LIBS + 'xml/xslt_processor');
+		var saxProcessor = require(SUPPORT_LIBS + 'xml/sax_processor');
+		
+		var xsltFile = XSLT_RES + 'transform_env.xslt';
+		var xsltFile_back = XSLT_RES + 'transform_env_reverse.xslt';
+		
+		//transform soap body to something that the real service can understand
+		var transformedMsg = xslt
+				.transformXML(zenithMessage.body, xsltFile, []);
+		
+		// create the soap message using original soap headers and transformed soap body
+		saxProcessor.getTransformedSOAP(zenithMessage.body, transformedMsg, function(err, transformedSOAP){
+			
+			zenithMessage.body = transformedSOAP;
+			endpoint.callService(zenithMessage, option, function(err, message) { 
+				
+				if(!err){
+					// transformed the response so that the client can understand the format.
+					var transformedBckMsg = xslt.transformXML(message.body, xsltFile_back, []);
+			
+					saxProcessor.getTransformedSOAP(message.body, transformedBckMsg, function(err, transformedBckSOAP){
+						message.body = transformedBckSOAP;
+						callback(null, message);
+					});
+					
+				} else {
+					//send error message for external web service error
+					//can create more detailed errors by reading fields in 'err' object
+					var errMsg = soapErrorMsg.getSOAP11Fault('Connection Refused.'); 
+					var errZenithMessage = zenithErrorMsg.getZenithErrorMSG(errMsg, 'text/xml', '503');
+					callback(null, errZenithMessage);
+				}
+				
+			});
+		});
+		
+
+	} else {
+		
+		var errMsg = oapErrorMsg.getSOAP11Fault('Invalid EPR value.'); 
+		var errZenithMessage = zenithErrorMsg.getZenithErrorMSG(errMsg, 'text/xml', '500');
+		callback(null, errZenithMessage);
+
+	}
+
+};
