3e263186b4c6d42dd6c70f5563931f0f337dba62
Gimantha Bandara <gmnth.bndr@gmail.com>
2013-11-07 14:41:07 +0530

    added CBRProxy loadtesting module.

--- samples/loadtest/cbr_proxy.js
+++ samples/loadtest/cbr_proxy.js
@@ -4,4 +4,31 @@
  * add the name of the WSDL and the service name
  * to  configuration/service_config.json
  */
+var cbrRoute = require('../../lib/support/cbr_route');
 
+exports.executeTest = function(zenithMessage, callback){
+	
+	var serviceURL = 'http://localhost:9000/service/EchoService';
+	var pathName = zenithMessage.transportHeaders.url.pathname;
+	
+	
+	//direct proxy
+	if(pathName === '/services/CBRProxy'){
+		
+		var option = {
+				url : serviceURL
+			};
+		cbrRoute.CBRSOAPBodyRoute(zenithMessage.body, '//order/symbol','IBM', function(){
+			
+			var endpoint = require(SUPPORT_LIBS + 'ws_endpoint');
+			
+			endpoint.callService(zenithMessage, option, function(err,message){		
+				callback(null, message);		
+			});	
+		});
+		
+	} 
+	
+	
+	
+};
