20e3e04bdf1a97c902b274a212801885fd67e811
chamilaadhi <cka.adhi@gmail.com>
2013-11-13 10:35:16 +0530

    edit error status

--- samples/sample_1.js
+++ samples/sample_1.js
@@ -6,6 +6,7 @@
 var SUPPORT_LIBS = '../lib/support/';
 var logger = require('../lib/logger');
 var soapErrorMsg = require('../lib/util/errormsg/soap_err_msg');
+var zenithErrorMsg = require('../lib/util/errormsg/zenith_err_msg');
 
 exports.executeSample = function(zenithMessage, callback){
 	var reqUrl = zenithMessage.transportHeaders.url; //returns url object
@@ -22,14 +23,22 @@ exports.executeSample = function(zenithMessage, callback){
 		var endpoint = require(SUPPORT_LIBS + 'ws_endpoint');
 			
 		endpoint.callService(zenithMessage, option, function(err,message){		
-				callback(null, message);		
+			if(!err){
+				
+				callback(null, message);	
+			} else {
+				//send error message for error situation
+				//can create more detailed errors by reading fields in 'err' object
+				var errMsg = soapErrorMsg.getSOAP11Fault('Connection Refused.'); 
+				var errZenithMessage = zenithErrorMsg.getZenithErrorMSG(errMsg, 'text/xml', '503');
+				callback(null, errZenithMessage);
+			}		
 			});	
 		
 	} else {
-		var errMsg = 'Invalid EPR value.'; 
-		zenithMessage.body = soapErrorMsg.getSOAP11Fault(errMsg);
-		callback(null, zenithMessage);
-		
+		var errMsg = soapErrorMsg.getSOAP11Fault('Invalid EPR value.'); 
+		var errZenithMessage = zenithErrorMsg.getZenithErrorMSG(errMsg, 'text/xml', '500');
+		callback(null, errZenithMessage);
 	}
 	
 }
