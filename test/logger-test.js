28da5565b2102eb935ffde8569dd5c7f0d3491d8
HasiniAbey <hvabey@gmail.com>
2013-11-22 02:14:01 +0530

    test cse to test log level

--- test/logger-test.js
+++ test/logger-test.js
@@ -2,44 +2,20 @@
  * test case for /lib/support/logger module
  */
 var vows = require('vows'), assert = require('assert');
-var logger = require('../../../../lib/support/logger');
+var logger = require('../configuration/log_config');
 
 var logMessage1 = '[2013-11-18 10:37:14.588] [INFO] [Zenith] -  Starting Zenith ESB ..... ';
 var logMessage2 = '[2013-11-18 10:38:45.425] [DEBUG] [ServiceView] - Start of View method';
 var logMessage3 = '[2013-11-18 10:39:56.392] [ERROR] [WSEndPoint] - problem with request: connect ECONNREFUSED';
 
-vows.decribe ('Zenith log message type INFO').addBatch({
-	'Logger' : {
-		topic:function(){
-			return logger.trace;
+vows.describe('Zenith message type').addBatch({
+	'Http Server configuration' : {
+		topic : function() {
+			return logger.logLevel;
 		},
-		'we get INFO': function(topic){
-			assert.equal(topic,'INFO');
-		}
-	}
-}
-		).run();
 
-vows.decribe ('Zenith log message type DEBUG').addBatch({
-	'Logger' : {
-		topic:function(){
-			return logger.debug;
-		},
-		'we get INFO': function(topic){
+		'we get DEBUG' : function(topic) {
 			assert.equal(topic,'DEBUG');
 		}
 	}
-}
-		).run();
-
-vows.decribe ('Zenith log message type ERROR').addBatch({
-	'Logger' : {
-		topic:function(){
-			return logger.trace;
-		},
-		'we get INFO': function(topic){
-			assert.equal(topic,'ERROR');
-		}
-	}
-}
-		).run();
+}).run();
