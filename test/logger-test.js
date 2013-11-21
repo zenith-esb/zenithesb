39c24115b8c9392593961885f4cfb958d9eaf19c
HasiniAbey <hvabey@gmail.com>
2013-11-21 15:27:31 +0530

    Added some test cases

--- test/logger-test.js
+++ test/logger-test.js
@@ -8,13 +8,37 @@ var logMessage1 = '[2013-11-18 10:37:14.588] [INFO] [Zenith] -  Starting Zenith 
 var logMessage2 = '[2013-11-18 10:38:45.425] [DEBUG] [ServiceView] - Start of View method';
 var logMessage3 = '[2013-11-18 10:39:56.392] [ERROR] [WSEndPoint] - problem with request: connect ECONNREFUSED';
 
-vows.decribe ('Zenith log message type').addBatch({
+vows.decribe ('Zenith log message type INFO').addBatch({
 	'Logger' : {
 		topic:function(){
 			return logger.trace;
 		},
 		'we get INFO': function(topic){
 			assert.equal(topic,'INFO');
+		}
+	}
+}
+		).run();
+
+vows.decribe ('Zenith log message type DEBUG').addBatch({
+	'Logger' : {
+		topic:function(){
+			return logger.debug;
+		},
+		'we get INFO': function(topic){
+			assert.equal(topic,'DEBUG');
+		}
+	}
+}
+		).run();
+
+vows.decribe ('Zenith log message type ERROR').addBatch({
+	'Logger' : {
+		topic:function(){
+			return logger.trace;
+		},
+		'we get INFO': function(topic){
+			assert.equal(topic,'ERROR');
 		}
 	}
 }
