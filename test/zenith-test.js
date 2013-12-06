6fbbcaa0050d45325c65f716bc47d7d78f33589f
HasiniAbey <hvabey@gmail.com>
2013-12-06 16:27:38 +0530

    Test Cases

--- test/zenith-test.js
+++ test/zenith-test.js
@@ -11,8 +11,35 @@ vows.describe('Zenith entry point').addBatch({
 			return serverConfig.transports[0].config.port;
 		},
 
-		'we get 8280' : function(topic) {
+		'The port is 8280' : function(topic) {
 			assert.equal(topic,'8280');
 		}
 	}
 }).run();
+
+//HTTPS configuration
+vows.describe('Zenith entry point').addBatch({
+	'Https configuration' : {
+		topic : function() {
+			return serverConfig.transports[1].config.port;
+		},
+
+		'The port is 8243' : function(topic) {
+			assert.equal(topic,'8243');
+		}
+	}
+}).run();
+
+//Monitor configuration
+vows.describe('Zenith entry point').addBatch({
+	'Monitor configuration' : {
+		topic : function() {
+			return serverConfig.monitor.port;
+		},
+
+		'The port is 3000' : function(topic) {
+			assert.equal(topic,'3000');
+		}
+	}
+}).run();
+
